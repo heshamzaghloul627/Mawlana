0966f53beff810e14f7177ab3106e2d85836b030
Hesham.Zaghloul <hesham.zaghloul@gotocme.com>
2026-03-22 19:12:31 +0200

    finalize admin

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,23 @@ const nextConfig = {
   trailingSlash: true,
   reactStrictMode: true,
   images: { unoptimized: true },
+  async rewrites() {
+    // In dev, proxy /api/* to Cloud Functions directly
+    return process.env.NODE_ENV === "production"
+      ? []
+      : [
+          {
+            source: "/api/:name*/",
+            destination:
+              "https://us-east1-thereturn-d3f2e.cloudfunctions.net/:name*",
+          },
+          {
+            source: "/api/:name*",
+            destination:
+              "https://us-east1-thereturn-d3f2e.cloudfunctions.net/:name*",
+          },
+        ];
+  },
 };
 
 export default nextConfig;
